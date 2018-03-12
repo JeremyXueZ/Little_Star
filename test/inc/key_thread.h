3673ddb27da8a0ce0a9be4a95984f166ffc638d5
stevenchen49 <1059301014@qq.com>
2018-03-12 14:11:38 +0800

    rm test

--- test/inc/key_thread.h
+++
@@ -1,6 +0,0 @@
-#ifndef KEY_THREAD_H
-#define KEY_THREAD_H
-
-void *key_thread_fn(void *arg);
-
-#endif
--- test/inc/play_thread.h
+++
@@ -1,6 +0,0 @@
-#ifndef PLAY_THREAD_H
-#define PLAY_THREAD_H
-
-void *play_thread_fn(void *arg);
-
-#endif
